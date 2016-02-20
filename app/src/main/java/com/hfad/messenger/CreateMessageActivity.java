6722844b1ab826a8ca3e7d3a6b78f7ce0cba8a59
javierguz10@gmail.com <Br3nD!t41n9n15>
2016-02-20 17:37:57 -0600

    cambios

--- app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -1,9 +1,11 @@
 package com.hfad.messenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -15,5 +17,14 @@ public class CreateMessageActivity extends Activity {
 
     public void onSendMessage(View view) {
 
+        EditText editText = (EditText) findViewById(R.id.message);
+        String text = editText.getText().toString();
+
+        System.out.println(text);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
@@ -1,13 +1,24 @@
 package com.hfad.messenger;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
+
+    public static final String MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String stringExtra = intent.getStringExtra(MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(stringExtra);
+
     }
 }
